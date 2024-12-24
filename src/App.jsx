9b0aa7f4fdea4bf9ce12351b1f9f9d4bfc57022e
MogesBekele <mogesbekele32@gmail.com>
2024-12-24 16:35:22 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h3 className='font-bold text-3xl text-center mx-auto my-auto justify-center items-center'>hello world</h3>
+      
+    </div>
+  )
+}
+
+export default App
